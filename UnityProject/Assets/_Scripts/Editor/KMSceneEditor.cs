63c53ab6e0882bd5dc1754369feaede4c1ce2b3c
pingzi1066 <wp2727@163.com>
2014-11-23 20:32:36 +0800

    Add: Scene editor sprite
    Method : Go to point.

---
+++ UnityProject/Assets/_Scripts/Editor/KMSceneEditor.cs
@@ -0,0 +1,35 @@
+﻿/******************************************************************************
+ *
+ * Maintaince Logs:
+ * 2014-11-23    WP   Initial version. 
+ *
+ * *****************************************************************************/
+
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class KMSceneEditor
+{
+    /// <summary>
+    /// 去到屏幕的某一点
+    /// </summary>
+    /// <param name="position"></param>
+    public static void GotoScenePoint(Vector3 position)
+    {
+        Object[] intialFocus = Selection.objects;
+        GameObject tempFocusView = new GameObject("Temp Focus View");
+        tempFocusView.transform.position = position;
+        try
+        {
+            Selection.objects = new Object[] { tempFocusView };
+            SceneView.lastActiveSceneView.FrameSelected();
+            Selection.objects = intialFocus;
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.ToString());
+        }
+        Object.DestroyImmediate(tempFocusView);
+    }
+}
