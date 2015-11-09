d9c016b87c586a38bdd41375b0cee580eda23824
wangping <wangping@chukong-inc.com>
2015-11-09 14:30:10 +0800

    计时器功能添加。

---
+++ UnityProject/Assets/_Scripts/Tools/Demo_KMTime.cs
@@ -0,0 +1,73 @@
+﻿/****************************************************************************** 
+ * 
+ * Maintaince Logs: 
+ * 2015-11-09     WP      Initial version
+ * 
+ * *****************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 计时器 Demo
+/// </summary>
+public class Demo_KMTime : MonoBehaviour
+{
+    private float timeScale = 0;
+    private float curTime = 0;
+    private float sumTime = 0;
+    private bool isFinished = true;
+
+    void OnGUI()
+    {
+        Time.timeScale = timeScale;
+        float left = 0;
+        float top = 0;
+        Rect rect = new Rect(left, top, 400, 30);
+        Rect rect1 = new Rect(left, top + 30, 400, 30);
+        GUI.TextField(rect, "Time Scale is " + timeScale);
+        timeScale = GUI.HorizontalScrollbar(rect1, timeScale, .2f, 0, 5);
+
+        Rect rect2 = new Rect(left, top + 60, 400, 30);
+        sumTime = GUI.HorizontalScrollbar(rect2, sumTime, .2f, 1, 20);
+        GUI.TextField(new Rect(left, top + 90, 400, 30), "sumTime is  " + sumTime);
+
+        if (isFinished)
+        {
+            if (GUI.Button(new Rect(left, top + 120, 100, 100), "Add"))
+            {
+                KMTime.AddTimeCount(sumTime, TestMethod);
+                isFinished = false;
+            }
+        }
+        else
+        {
+            GUI.TextField(new Rect(left, top + 120, 400, 100), "cur Time is  " + curTime);
+        }
+
+    }
+
+    void TestMethod(float cur , float sum)
+    {
+        curTime = cur;
+        sumTime = sum;
+        //Debug.Log(cur + " ----------- " + sum);
+
+        if (cur >= sum)
+        {
+            isFinished = true;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- UnityProject/Assets/_Scripts/Tools/KMTime.cs
+++ UnityProject/Assets/_Scripts/Tools/KMTime.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 计时器委托
+/// </summary>
+/// <param name="curTime">当前时间</param>
+/// <param name="sumTime">总时间</param>
+public delegate void DelTimeCount(float curTime, float sumTime);
 
 /// <summary>
 /// 描述 忽略TimeScale 而提供的一些时间 参数
@@ -7,11 +14,24 @@ using System.Collections;
 /// Maintaince Logs:
 /// 2015-03-30	WP			Initial version. 
 /// 2015-08-21  WP          update....by unity api ,add scale,rename class to KMTime
+/// 2015-11-09  WP          添加计时功能
 /// </summary>
 public class KMTime : MonoBehaviour
 {
-    private static KMTime mInst;
+    private static KMTime mInstance;
+    private static KMTime mInst
+    {
+        get
+        {
+            if (mInstance == null) Spawn();
+            return mInstance;
+        }
+    }
     private float mRealTime = 0f;
+    /// <summary>
+    /// 计时器方法列表
+    /// </summary>
+    private List<KMTimeCount> listTimeCount = new List<KMTimeCount>();
 
     static private float m_timeScale = 1;
     static public float timeScale
@@ -35,7 +55,38 @@ public class KMTime : MonoBehaviour
     {
         GameObject go = new GameObject("_KMTime");
         DontDestroyOnLoad(go);
-        mInst = go.AddComponent<KMTime>();
+        mInstance = go.AddComponent<KMTime>();
+    }
+
+    static public void AddTimeCount(float time, DelTimeCount method)
+    {
+        KMTimeCount tc = new KMTimeCount(time, method);
+        mInst.listTimeCount.Add(tc);
+    }
+
+    private class KMTimeCount
+    {
+        private float timeSum = 1;
+        private float timeParam = 0;
+        private DelTimeCount eventTimeCount;
+
+        public KMTimeCount(float sum, DelTimeCount method)
+        {
+            timeSum = sum;
+            eventTimeCount = method;
+        }
+
+        public bool Update()
+        {
+            if (timeParam >= timeSum) return true;
+
+            timeParam += KMTime.deltaTime;
+            timeParam = Mathf.Min(timeParam, timeSum);
+
+            if (eventTimeCount != null) eventTimeCount(timeParam, timeSum);
+
+            return false;
+        }
     }
 
 #if UNITY_4_3
@@ -53,7 +104,6 @@ public class KMTime : MonoBehaviour
 #if UNITY_EDITOR
 			if (!Application.isPlaying) return Time.realtimeSinceStartup;
 #endif
-			if (mInst == null) Spawn();
 			return mInst.mRealTime;
 		}
 	}
@@ -69,7 +119,6 @@ public class KMTime : MonoBehaviour
 #if UNITY_EDITOR
 			if (!Application.isPlaying) return 0f;
 #endif
-			if (mInst == null) Spawn();
 			return mInst.mRealDelta;
 		}
 	}
@@ -88,7 +137,6 @@ public class KMTime : MonoBehaviour
     {
         get
         {
-            if (mInst == null) Spawn();
             return mInst.mRealTime;
         }
     }
@@ -102,6 +150,16 @@ public class KMTime : MonoBehaviour
     void Update()
     {
         mRealTime += deltaTime;
+
+        for (int i = 0; i < listTimeCount.Count; )
+        {
+            if (listTimeCount[i].Update())
+            {
+                listTimeCount.RemoveAt(i);
+                continue;
+            }
+            i++;
+        }
     }
 #endif
 }
