68740c76b5bc2841d26fed535cc38adba8b4765f
WonPeace <wp2727@163.com>
2016-06-05 23:43:25 +0800

    代码续传。加入了python转json工具。

---
+++ UnityProject/Assets/_Scripts/GameSystem/TimingReward/ModelTimingReward.cs
@@ -0,0 +1,18 @@
+/****************************************************************************** 
+ * 
+ * Maintaince Logs: 
+ * 2016-06-05     WP      Initial version
+ * 
+ * *****************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 时间奖励取表模版
+/// </summary>
+public class ModelTimingReward : ModelBase<StaticTimingReward> 
+{
+
+	
+}
---
+++ UnityProject/Assets/_Scripts/GameSystem/TimingReward/StaticTimingReward.cs
@@ -0,0 +1,17 @@
+/****************************************************************************** 
+ * 
+ * Maintaince Logs: 
+ * 2016-06-05     WP      Initial version
+ * 
+ * *****************************************************************************/
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 时间奖励的表结构
+/// </summary>
+public class StaticTimingReward : StaticJson<StaticTimingReward> 
+{
+
+}
