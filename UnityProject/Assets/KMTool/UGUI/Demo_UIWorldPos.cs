bbe5cffa70d7d5ad3badb4afd7a85e3ce31258b3
WonPeace <wonpeace@WonPeacedeMBP.lan>
2016-12-09 22:25:26 +0800

    added 3D物体到UI坐标。

---
+++ UnityProject/Assets/KMTool/UGUI/Demo_UIWorldPos.cs
@@ -0,0 +1,59 @@
+/****************************************************************************** 
+ * 
+ * Maintaince Logs: 
+ * 2016-12-09     WP      Initial version
+ * 
+ * *****************************************************************************/
+
+using KMTool;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 描述
+/// </summary>
+public class Demo_UIWorldPos : MonoBehaviour 
+{
+
+    [SerializeField] private GameObject go;
+     
+    private Vector3 toPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void BtnToUI()
+    {
+        toPos = UIWorldPos.GetPos("demo", transform);
+        LerpTools.instance.AddElement(new LerpTools.Element(0, 1, 1, true), Move);
+
+    }
+
+    public void Move(float cur , Color col)
+    {
+        transform.position = Vector3.Lerp(transform.position, toPos, cur);
+    }
+
+    #region 测试
+
+    public void KMDebug()
+    {
+        Debug.Log(" ---------KMDebug----------", gameObject);
+    }
+
+    public void KMEditor()
+    {
+        Debug.Log(" ---------KMEditor----------", gameObject);
+    }
+
+    #endregion
+}
---
+++ UnityProject/Assets/KMTool/UGUI/UIWorldPos.cs
@@ -0,0 +1,130 @@
+/****************************************************************************** 
+ * 
+ * Maintaince Logs: 
+ * 2016-12-02     WP      Initial version
+ * 
+ * *****************************************************************************/
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace KMTool
+{
+
+[ExecuteInEditMode]
+    /// <summary>
+/// UGUI 转世界坐标
+/// </summary>
+    public class UIWorldPos : MonoBehaviour 
+    {
+        /// <summary>
+        /// 保存UI名字与坐标的字典
+        /// </summary>
+        private static Dictionary<string ,UIWorldPos> dictWorldPos = new Dictionary<string, UIWorldPos>();
+
+        [SerializeField] private string posName = "new pos name";
+
+        [SerializeField][DisableEdit] private Vector3 curWorldPos;
+
+        [SerializeField][DisableEdit] private Vector3 prePos;
+
+        void Awake()
+        {
+            AddToDict(posName, this);
+
+            UpdatePos();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        [ContextMenu("Init")]
+        void UpdatePos()
+        {
+            if (prePos != transform.position)
+            {
+                prePos = transform.position;
+                curWorldPos = ConvertToWorldPos(Camera.main, prePos);
+
+    //            Debug.Log(transform.InverseTransformPoint(transform.position) + "    " + RectTransformUtility.PixelAdjustPoint);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdatePos();
+        }
+
+        void OnDestroy()
+        {
+            RemoveToDict(posName);
+        }
+
+        private void AddToDict(string name,UIWorldPos pos)
+        {
+            if (!dictWorldPos.ContainsKey(name))
+            {
+                dictWorldPos.Add(name, pos);
+            }
+            else
+            {
+                dictWorldPos[name] = pos;
+            }
+        }
+
+        private void RemoveToDict(string name)
+        {
+            if (dictWorldPos.ContainsKey(name))
+            {
+                dictWorldPos.Remove(name);
+            }
+        }
+
+        static public Vector3 ConvertToWorldPos(Camera cam,Vector3 pos,float z = 5)
+        {
+            pos.z = z;//Mathf.Clamp(pos.z, cam.nearClipPlane, cam.farClipPlane);
+            Vector3 worldPos = cam.ScreenToWorldPoint(pos);
+
+            return worldPos;
+        }
+
+        static public UIWorldPos Get(string name)
+        {
+            if (dictWorldPos.ContainsKey(name))
+                return dictWorldPos[name];
+
+            Debug.LogError("Don't find to ui pos " + name);
+            return null;
+        }
+
+        static public Vector3 GetPos(string name, Transform target)
+        {
+            Vector3 uiPos = Get(name).transform.position;
+            return ConvertToWorldPos(Camera.main, uiPos, Vector3.Distance(target.position, Camera.main.transform.position));
+        }
+
+        #region 测试
+
+        protected void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(curWorldPos, 1);
+        }
+
+        public void KMDebug()
+        {
+            Debug.Log(" ---------KMDebug----------", gameObject);
+        }
+
+        public void KMEditor()
+        {
+            Debug.Log(" ---------KMEditor----------", gameObject);
+        }
+
+        #endregion
+    }
+}
