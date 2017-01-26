5e2d8c12e553baf7c64d2540ab2f6fc6ae8b36e5
WonPeace <wonpeace@WonPeacedeMBP.lan>
2017-01-26 23:36:14 +0800

    - EnumTools 加入HasFlags方法

--- UnityProject/Assets/KMTool/Tools/Enum/EnumTools.cs
+++ UnityProject/Assets/KMTool/Tools/Enum/EnumTools.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 /// 
 /// Maintaince Logs: 
 /// 2015-07-16		WP			Initial version.
+/// 2017-01-26      WP          added HasFlag method for all enum 
 public static class EnumTools
 {
     public static string GetDescription(Enum obj)
@@ -28,4 +29,31 @@ public static class EnumTools
         return array;
     }
 
+    /// <summary>
+    /// Check to see if a flags enumeration has a specific flag set.
+    /// http://stackoverflow.com/questions/4108828/generic-extension-method-to-see-if-an-enum-contains-a-flag
+    /// </summary>
+    /// <param name="variable">Flags enumeration to check</param>
+    /// <param name="value">Flag to check for</param>
+    /// <returns></returns>
+    public static bool HasFlag(this Enum variable, Enum value)
+    {
+        if (variable == null)
+            return false;
+
+        if (value == null)
+            throw new ArgumentNullException("value");
+
+        // Not as good as the .NET 4 version of this function, but should be good enough
+        if (!Enum.IsDefined(variable.GetType(), value))
+        {
+            throw new ArgumentException(string.Format(
+                "Enumeration type mismatch.  The flag is of type '{0}', was expecting '{1}'.",
+                value.GetType(), variable.GetType()));
+        }
+
+        ulong num = Convert.ToUInt64(value);
+        return ((Convert.ToUInt64(variable) & num) == num);
+
+    }
 }
