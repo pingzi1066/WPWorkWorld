e9337bb7a1e7c8cbb7573fc3751bbc2c41592157
WonPeace <wp27272@163.com>
2017-02-09 17:25:31 +0800

    fixed UIWordPos： 在Canvas为 ScreenSpace - Camera 的时候，坐标对应不正确

--- UnityProject/Assets/KMTool/UGUI/UIWorldPos.cs
+++ UnityProject/Assets/KMTool/UGUI/UIWorldPos.cs
@@ -12,22 +12,27 @@ using System.Collections.Generic;
 namespace KMTool
 {
 
-[ExecuteInEditMode]
+    [ExecuteInEditMode]
     /// <summary>
-/// UGUI 转世界坐标
-/// </summary>
-    public class UIWorldPos : MonoBehaviour 
+    /// UGUI 转世界坐标
+    /// </summary>
+    public class UIWorldPos : MonoBehaviour
     {
         /// <summary>
         /// 保存UI名字与坐标的字典
         /// </summary>
-        private static Dictionary<string ,UIWorldPos> dictWorldPos = new Dictionary<string, UIWorldPos>();
+        private static Dictionary<string, UIWorldPos> dictWorldPos = new Dictionary<string, UIWorldPos>();
 
-        [SerializeField] private string posName = "new pos name";
+        [SerializeField]
+        private string posName = "new pos name";
 
-        [SerializeField][DisableEdit] private Vector3 curWorldPos;
+        [SerializeField]
+        [DisableEdit]
+        private Vector3 curWorldPos;
 
-        [SerializeField][DisableEdit] private Vector3 prePos;
+        [SerializeField]
+        [DisableEdit]
+        private Vector3 prePos;
 
         void Awake()
         {
@@ -47,9 +52,15 @@ namespace KMTool
             if (prePos != transform.position)
             {
                 prePos = transform.position;
+                Canvas c = transform.root.GetComponent<Canvas>();
+                if (c != null && c.renderMode == RenderMode.ScreenSpaceCamera)
+                {
+                    curWorldPos = transform.position;
+                    return;
+                }
                 curWorldPos = ConvertToWorldPos(Camera.main, prePos);
 
-    //            Debug.Log(transform.InverseTransformPoint(transform.position) + "    " + RectTransformUtility.PixelAdjustPoint);
+                //            Debug.Log(transform.InverseTransformPoint(transform.position) + "    " + RectTransformUtility.PixelAdjustPoint);
             }
         }
 
@@ -64,7 +75,7 @@ namespace KMTool
             RemoveToDict(posName);
         }
 
-        private void AddToDict(string name,UIWorldPos pos)
+        private void AddToDict(string name, UIWorldPos pos)
         {
             if (!dictWorldPos.ContainsKey(name))
             {
@@ -84,7 +95,7 @@ namespace KMTool
             }
         }
 
-        static public Vector3 ConvertToWorldPos(Camera cam,Vector3 pos,float z = 5)
+        static public Vector3 ConvertToWorldPos(Camera cam, Vector3 pos, float z = 5)
         {
             pos.z = z;//Mathf.Clamp(pos.z, cam.nearClipPlane, cam.farClipPlane);
             Vector3 worldPos = cam.ScreenToWorldPoint(pos);
@@ -103,8 +114,10 @@ namespace KMTool
 
         static public Vector3 GetPos(string name, Transform target)
         {
-            Vector3 uiPos = Get(name).transform.position;
-            return ConvertToWorldPos(Camera.main, uiPos, Vector3.Distance(target.position, Camera.main.transform.position));
+            //Vector3 uiPos = Get(name).transform.position;
+            //return ConvertToWorldPos(Camera.main, uiPos, Vector3.Distance(target.position, Camera.main.transform.position));
+            Vector3 pos = Get(name).curWorldPos;
+            return new Vector3(pos.x, pos.y, target.position.z);
         }
 
         #region 测试
