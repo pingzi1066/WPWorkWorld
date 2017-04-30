f5dfe1431bdc2b4f448bcbcaf38d340838df302f
WonPeace <wp2727@163.com>
2017-04-30 16:02:07 +0800

    added py转Excel 多Sheet支持

--- Python/script/convertExcel.py
+++ Python/script/convertExcel.py
@@ -4,11 +4,12 @@
 @ copyRight WonPeace  
 
 Maintaince Logs: 
-2015-08-01 Initial version
-
+2015-08-01 	Initial version
+2017-04-30	封装判断，去掉相同的代码
+			加入了Sheet全部转换的代码
 
 '''
-import sys,os,excel2json,kmTools
+import sys,os,excel2json,kmTools, xlrd
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -43,25 +44,12 @@ def convert(excelPath, exportPath, recordPath, exportNewPath):
 				#检查记录中是否已经是导出过最新的文件
 				curMd5 = kmTools.getFileMd5(excelPath + f)
 
-				if str(inputStr) == 'yes':
-					outputFilePath = exportPath + "Static" + fname[0:1].upper() + fname[1:] + ".json"
-					outputFile = open(outputFile,'w')
-					excel2json.excelToJsonById(excelPath + f,outputFile, [2])
-					#复制到新文件夹
-					kmTools.copy_file(outputFilePath,exportNewPath)
-
-					recordFile = kmTools.openFile(recordPath + fname + ".txt",'w')
-					recordFile.write(curMd5)
-					recordFile.close()
-					continue
-
 				recordFile = kmTools.openFile(recordPath + fname + ".txt",'r')
 				preMd5 = recordFile.read()
 				recordFile.close()
 
-				if curMd5 == preMd5:
-					print  " -file is not change!!!!!- " + f
-				else :
+				# 写入条件：用户输入重导入 或者 判断时文件有所改变
+				if curMd5 != preMd5 or str(inputStr) == 'yes':
 					outputFilePath = exportPath + "Static" + fname[0:1].upper() + fname[1:] + ".json"
 					outputFile = open(outputFilePath,'w')
 
@@ -73,3 +61,76 @@ def convert(excelPath, exportPath, recordPath, exportNewPath):
 					recordFile = kmTools.openFile(recordPath + fname + ".txt",'w')
 					recordFile.write(curMd5)
 					recordFile.close()
+				else :
+					print  " -file is not change!!!!!- " + f
+
+'''
+转换一个Excel的所有的Sheet
+每个单独的表导出后，
+第一个表的名字为默认名字
+后面的名字为原来名字 + Sheet名字（Sheet首字母大写）
+其它的逻辑保持一样
+'''
+def convertAllSheet(excelPath, exportPath, recordPath, exportNewPath):
+	inputStr = raw_input("export all excel please enter input: yes\n")
+
+	#清空新导出的new文件夹
+	kmTools.del_dir(exportNewPath)
+	kmTools.mkdir(exportNewPath)
+
+	#先创建相关的目录
+	kmTools.mkdir(recordPath)
+	kmTools.mkdir(exportPath)
+
+	#从根目录获得所有的分目录、文件名。root 为地址，dirs 为地址下的文件夹 、 files为地址下的文件
+	for root, dirs, files in os.walk(excelPath):
+		for f in files:
+			#取文件名 + 后缀
+			fname, fextension = os.path.splitext(f)
+			#是表格文件
+			if(fextension == ".xlsx" or fextension == ".csv" or fextension == ".xls"):
+
+				excelName = excelPath + f
+				#检查记录中是否已经是导出过最新的文件
+				curMd5 = kmTools.getFileMd5(excelName)
+
+				recordFile = kmTools.openFile(recordPath + fname + ".txt",'r')
+				preMd5 = recordFile.read()
+				recordFile.close()
+
+				# 写入条件：用户输入重导入 或者 判断时文件有所改变
+				if curMd5 != preMd5 or str(inputStr) == 'yes':
+					#导出第一个表格
+					outputFilePath = exportPath + "Static" 
+							+ fname[0:1].upper() + fname[1:] 
+							+ sheetName[0:1].upper() + sheetName[1:]
+							+ ".json"
+
+					outputFile = open(outputFilePath,'w')
+
+					excel2json.excelToJsonById(excelName, outputFile, [2])
+					kmTools.copy_file(outputFilePath,exportNewPath)
+
+					#导出其它表格
+					excelFile = xlrd.open_workbook(excelName)
+					for sheet_index in range(1, excelFile.nsheets): 
+
+						sheet = excelFile.sheet_by_index(sheet_index)
+						sheetName = sheet.name
+
+						outputFilePath = exportPath + "Static" 
+							+ fname[0:1].upper() + fname[1:] 
+							+ sheetName[0:1].upper() + sheetName[1:]
+							+ ".json"
+						outputFile = open(outputFilePath,'w')
+
+						excel2json.sheetToJson(sheet, outputFile, [2], True)
+
+						#复制到新文件夹
+						kmTools.copy_file(outputFilePath,exportNewPath)
+
+					recordFile = kmTools.openFile(recordPath + fname + ".txt",'w')
+					recordFile.write(curMd5)
+					recordFile.close()
+				else :
+					print  " -file is not change!!!!!- " + f
